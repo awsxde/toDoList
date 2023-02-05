fcefdf784fb2281c15a070999355863022535f31
awsxde <ali23khour@gmail.com>
2023-02-05 17:11:01 +0330

    finishing dark mode

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-let input = document.querySelector(".input");
-let add = document.querySelector(".submit");
-let clear = document.querySelector(".clear");
-let tasks = document.querySelector(".tasks");
-
-let darkMode = document.querySelector(".theme");
-let body = document.querySelector("body");
-let isDark = false;
-
-darkMode.addEventListener("click", () => {
-  let special = document.querySelectorAll(".special");
-  special.forEach((i) => i.classList.add("dark"));
-  isDark = true;
-});
+const selectAll = (x) => document.querySelectorAll(`.${x}`);
+const select = (x) => document.querySelector(`.${x}`);
 
 let count = 0;
+let isDark = false;
+let add = select("submit");
+let input = select("input");
+let clear = select("clear");
+let tasks = select("tasks");
+let darkMode = select("theme");
+
+function darkModeOn() {
+  selectAll("special").forEach((i) => i.classList.add("dark"));
+  isDark = true;
+}
+
+function darkModeOff() {
+  selectAll("dark").forEach((i) => i.classList.remove("dark"));
+  isDark = false;
+}
+
+function darkModeHandler() {
+  isDark ? darkModeOff() : darkModeOn();
+}
 
 function addCard(val, n) {
   tasks.insertAdjacentHTML(
@@ -22,7 +30,7 @@ function addCard(val, n) {
     <div class="task task-${n} ${isDark ? "dark" : "special"}">
       <p class="text">${val}</p>
         <button class="del del-${n} ${isDark ? "dark" : "special"}">
-          <img src="./trash-outline.svg" alt="trash" class="trash" />
+          <img src="./img/trash-outline.svg" alt="trash" class="trash" />
         </button>
     </div>
     `
@@ -30,13 +38,13 @@ function addCard(val, n) {
 }
 
 function delHandler() {
-  document.querySelectorAll(".del").forEach((i) => {
+  selectAll("del").forEach((i) => {
     i.addEventListener("click", () => del(i.classList[1].slice(4)));
   });
 }
 
 function del(n) {
-  document.querySelector(`.task-${n}`).classList.add("hide");
+  select(`task-${n}`).classList.add("hide");
 }
 
 function submitHandler() {
@@ -50,7 +58,9 @@ function submitHandler() {
 
 function clearHandler() {
   tasks.innerHTML = "";
+  input.value = "";
 }
 
 add.addEventListener("click", submitHandler);
 clear.addEventListener("click", clearHandler);
+darkMode.addEventListener("click", darkModeHandler);
